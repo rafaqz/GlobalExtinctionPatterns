68a7ce27675ceae0cd1d27de80a5baf1b81f6868
Rafael Schouten <rafaelschouten@gmail.com>
2024-08-23 18:42:33 +0200

    simple model of everything with a plot

--- scripts/turing.jl
+++ scripts/turing.jl
@@ -5,6 +5,18 @@ using Revise
 
 mass_df = load_mass_table()
 
+
+@model function simple_extincion_mass(mass, year, year_squared)
+    b1 ~ Normal(0, 100)
+    b2 ~ Normal(0, 100)
+    b3 ~ Normal(0, 100)
+    sigma ~ Exponential(10)
+    intercept ~ Normal(0, 100)
+ 
+    mass_est = intercept .+ year .* b1 .+ year_squared .* b2 
+    mass ~ MvNormal(mass_est, sigma)        
+end
+
 @model function extincion_mass(mass, year, year_squared, wasuninhabited, colonised_year)
     b1 ~ Normal(0, 100)
     b2 ~ Normal(0, 100)
@@ -33,40 +45,59 @@ since_colonised = (mass_df.yearLastSeen_cleaned .- mass_df.colonised) ./ 250
 
 year_mass_all = extincion_mass(mass, year, year_squared, wasuninhabited, colonised_year)
 chain = sample(year_mass_all, NUTS(), 1000)
-chain
+
+year_mass_simple = simple_extincion_mass(mass, year, year_squared)
+simple_chain = sample(year_mass_simple, NUTS(), 1000)
 
 years = ((1500:2000) .- 1750) ./ 250
 years_squared = years .^ 2
-b1 = chain[:b1] 
-b2 = chain[:b2]
-b3 = chain[:b3]
-b4 = chain[:b4]
-b5 = chain[:b5]
-# b6 = chain[:b6]
-# b7 = chain[:b7]
-intercept = chain[:intercept]
-chain
 
-# mass_est = intercept .+ years' .* b1 .+ (years .^ 2)' .* b2
-fig = Figure()
-axs = []
-for (i, wasuninhabited) in enumerate([false, true]), (j, colonised_year) in  enumerate([-0.5, 0, 0.5])
-    xlabel = i == 2 ? string(colonised_year * 250 + 1750) : "" 
-    ylabel = j == 1 ? (wasuninhabited ? "uninhabited" : "inhabited") : ""
-    ax = Axis(fig[i, j]; xlabel, ylabel)
+fig = let
+    fig = Figure()
+    axs = []
+    for (i, wasuninhabited) in enumerate([false, true]), (j, colonised_year) in  enumerate([-0.5, 0, 0.5])
+        b1 = chain[:b1] 
+        b2 = chain[:b2]
+        b3 = chain[:b3]
+        b4 = chain[:b4]
+        b5 = chain[:b5]
+        intercept = chain[:intercept]
+        xlabel = i == 2 ? string(colonised_year * 250 + 1750) : "" 
+        ylabel = j == 1 ? (wasuninhabited ? "uninhabited" : "inhabited") : ""
+        ax = Axis(fig[i, j]; xlabel, ylabel, yscale=log10)
+        push!(axs, ax)
+        # mass_est = intercept .+ year .* b1 .+ year_squared .* b2 .+ wasuninhabited .* b3 .+ 
+            # wasuninhabited .* year_squared .* b4 .+ colonised_year .* wasuninhabited .* year .* b5
+        mass_est = intercept .+ years' .* b1 .+ years_squared' .* b2 .+ wasuninhabited .* b3 .+ 
+            wasuninhabited .* years_squared' .* b4 .+ colonised_year .* years_squared' .* b5
+        mass_est_mean = mean(mass_est; dims=1)
+        mass_est_quant025 = quantile.(eachcol(mass_est), Ref(0.025))
+        mass_est_quant975 = quantile.(eachcol(mass_est), Ref(0.975))
+
+        p = Makie.plot!(ax, 1500:2000, exp.(vec(mass_est_mean)))
+        Makie.band!(ax, 1500:2000, exp.(mass_est_quant025), exp.(mass_est_quant975); alpha=0.7)
+    end
+    linkaxes!(axs...)
+end
+
+fig = let
+    b1 = simple_chain[:b1] 
+    b2 = simple_chain[:b2]
+    b3 = simple_chain[:b3]
+    intercept = simple_chain[:intercept]
+    fig = Figure()
+    axs = []
+    ax = Axis(fig[1, 1]; yscale=log10)
     push!(axs, ax)
-    # mass_est = intercept .+ year .* b1 .+ year_squared .* b2 .+ wasuninhabited .* b3 .+ 
-        # wasuninhabited .* year_squared .* b4 .+ colonised_year .* wasuninhabited .* year .* b5
-    mass_est = intercept .+ years' .* b1 .+ years_squared' .* b2 .+ wasuninhabited .* b3 .+ 
-        wasuninhabited .* years_squared' .* b4 .+ colonised_year .* years_squared' .* b5
+    mass_est = intercept .+ years' .* b1 .+ years_squared' .* b2
     mass_est_mean = mean(mass_est; dims=1)
     mass_est_quant025 = quantile.(eachcol(mass_est), Ref(0.025))
     mass_est_quant975 = quantile.(eachcol(mass_est), Ref(0.975))
 
-    p = Makie.plot!(ax, 1500:2000, vec(mass_est_mean))
-    Makie.band!(ax, 1500:2000, mass_est_quant025, mass_est_quant975)
+    p = Makie.plot!(ax, 1500:2000, exp.(vec(mass_est_mean)))
+    Makie.band!(ax, 1500:2000, exp.(mass_est_quant025), exp.(mass_est_quant975); alpha=0.7)
+    Makie.scatter!(ax, mass_df.yearLastSeen_cleaned, exp.(mass); alpha=0.5)
+    fig
 end
-linkaxes!(axs...)
-
 
 
